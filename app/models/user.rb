3e1ef94825718f0908678e6224c082f30eadc805
Shalin <shalin.bhatt@hotmail.com>
2017-01-23 08:42:19 -0500

    Login page UI changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :tasks, dependent: :destroy
+  #validates_presence_of :user, :password
 end
